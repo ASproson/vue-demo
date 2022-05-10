f83cd9089bce4afdcc8a7b643e11dc92b1e7b782
Atlas Sproson <atlas.sproson@r2-factory.com>
2022-05-10 13:25:24 +0100

    Adds base demo layout with Header

--- demo-app/src/App.vue
+++ demo-app/src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <Header />
+      <h1>Home Page</h1>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    Header,
   },
 
   data: () => ({
     //
   }),
-}
+};
 </script>
+
+<style>
+h1 {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
---
+++ demo-app/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar color="#0083cb" height="50">
+    <v-toolbar-title id="header-title">Demo Header</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<style>
+#header-title {
+  color: white;
+}
+</style>
